16d0df3e659700c16be87331125d8f07e1aeed9f
aislan rafael <aislanmaster@gmail.com>
2018-08-17 14:35:00 -0300

    testando conexão com o banco de dados

---
+++ src/br/edu/ifpi/ads3/pc/jdbc/TesteConexao.java
@@ -0,0 +1,27 @@
+package br.edu.ifpi.ads3.pc.jdbc;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TesteConexao {
+	public static void main(String[] args) throws SQLException {
+		Connection c = DriverManager.getConnection("jdbc:hsqldb:hsql://"
+				+ "localhost/loja-virtual", "SA", "");
+		Statement stmt = c.createStatement();
+		stmt.execute("select * from produto");
+		ResultSet resultSet = stmt.getResultSet();
+		while (resultSet.next()) {
+			int id = resultSet.getInt("id");
+			String nome = resultSet.getString("nome");
+			String descricao = resultSet.getString("descricao");
+			System.out.println("id " + id);
+			System.out.println("Nome " + nome);
+			System.out.println("Descricao " + descricao);
+		}
+		c.close();
+	}
+}
